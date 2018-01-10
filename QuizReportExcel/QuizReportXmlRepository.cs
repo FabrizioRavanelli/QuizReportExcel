4d96451a3a8fafbd938acbf29043d21117cfad0c
MrPinedita <sigfridcouto@gmail.com>
2018-01-10 08:19:28 +0100

    Clean + ReadFile & ReadFolder

--- QuizReportExcel/QuizReportXmlRepository.cs
+++ QuizReportExcel/QuizReportXmlRepository.cs
@@ -5,6 +5,7 @@ using System.Xml.Linq;
 
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace QuizReportExcel
 {
@@ -13,13 +14,30 @@ namespace QuizReportExcel
         public void ReadFolder(string path)
         {
             //foreach file_in_path => readFile
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            DirectoryInfo d = new DirectoryInfo(filepath);
+            //TODO Anpassen....
+            foreach (var file in d.GetFiles("*.xml"))
+            {
+                Directory.Move(file.FullName, filepath + "\\TextFiles\\" + file.Name);
+            }
         }
 
         public void ReadFile(string pathOfQuizReportXml)
         {
             var document = XDocument.Load(pathOfQuizReportXml, LoadOptions.SetBaseUri);
             //LogToAvailableLoggers("Öffne Zuordnungsdatei...", LogMessageType.Info, session, Task.PressImport);
-            var mappingDocument = XDocument.Load(@"PressImportXmlObjectMappingConfig.xml", LoadOptions.SetBaseUri);
+            var questions = document.Root.Element("questions").Elements("multipleChoiceQuestion");
+            foreach (var question in questions)
+            {
+                var id = question.Attribute("id");
+                var status = question.Attribute("status");
+                var correctAnswerIndex = question.Element("answers").Attribute("correctAnswerIndex");
+                var userAnswerIndex = question.Element("answers").Attribute("userAnswerIndex");
+                
+                Console.WriteLine("multipleChoiceQuestion: id={0}, status={1}, correctAnswerIndex={2}, userAnswerIndex={3}," +
+                    id, status, correctAnswerIndex, userAnswerIndex);
+            }
         }
     }
 }
